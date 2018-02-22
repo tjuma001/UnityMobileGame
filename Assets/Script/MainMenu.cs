2f7adc5fe2347cf941a0c17681e329e1af3ee294
Tariq <tjuma001@fiu.edu>
2018-02-21 23:45:37 -0500

    Marble color changes

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    private const float CAMERA_TRANSITION_SPEED = 3.0f;
+
+    public GameObject levelButtonPrefab;
+    public GameObject levelButtonContainer;
+
+    public GameObject shopButtonPrefab;
+    public GameObject shopButtonContainer;
+
+    public Material playerMaterial;
+
+    private Transform cameraTransform;
+    private Transform cameraDesiredLookAt;
+
+    private void Start()
+    {
+        ChangePlayerSkin(10);
+        cameraTransform = Camera.main.transform;
+        Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
+        foreach(Sprite thumbnail in thumbnails)
+        {
+            GameObject container = Instantiate(levelButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = thumbnail;
+            container.transform.SetParent(levelButtonContainer.transform, false);
+
+            string sceneName = thumbnail.name;
+            container.GetComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
+        }
+
+        int textureIndex = 0;
+        Sprite[] textures = Resources.LoadAll<Sprite>("Player");
+        foreach(Sprite texture in textures)
+        {
+            GameObject container = Instantiate(shopButtonPrefab) as GameObject;
+            container.GetComponent<Image>().sprite = texture;
+            container.transform.SetParent(shopButtonContainer.transform, false);
+
+            int index = textureIndex;
+            container.GetComponent<Button>().onClick.AddListener(() => ChangePlayerSkin(index));
+            textureIndex++;
+        }
+    }
+
+    private void Update()
+    {
+        if(cameraDesiredLookAt != null)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, CAMERA_TRANSITION_SPEED * Time.deltaTime);
+        }
+    }
+
+    private void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LookAtMenu(Transform menuTransform)
+    {
+        cameraDesiredLookAt = menuTransform;
+    }
+
+    private void ChangePlayerSkin(int index)
+    {
+        float x = (index % 4) * 0.25f;
+        float y = ((int)index / 4) * 0.25f;
+
+        if(y == 0.0f)
+        {
+            y = 0.75f;
+        }
+        else if(y == 0.25f)
+        {
+            y = 0.5f;
+        }
+        else if(y == 0.50f)
+        {
+            y = 0.25f;
+        }
+        else if(y == 0.75f)
+        {
+            y = 0f;
+        }
+
+        playerMaterial.SetTextureOffset("_MainTex", new Vector2(x,y));
+    }
+
+}
